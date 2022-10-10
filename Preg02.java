eb5f75d12f03584d7659f018b3f02ec8c7585f09
Kevin Caman <kevingabriel092001@gmail.com>
2022-10-10 09:12:46 -0500

    Pregunta02 - Listo

---
+++ Preg02.java
@@ -0,0 +1,15 @@
+#include "iostream"
+using namespace std;
+
+int main()
+{
+int a = 0;
+int b = 0;
+cout << " Datos de entrada: "<<endl;
+cout << " A: ";cin>>a;
+cout << " B: ";cin>>b;
+
+cout << " Datos de salida: "<<endl;
+cout << " A: "<<b<<endl;
+cout << " B: "<<a;
+}
